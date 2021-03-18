55fbfb4984686f327c7b31d70123bbc6a725b998
noname2048 <sungwook.csw@gmail.com>
2021-03-18 22:20:47 +0900

    aws s3 fixed

--- django_project/mysite/urls.py
+++ django_project/mysite/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),  # admin
@@ -27,3 +29,7 @@ urlpatterns += [
     path("accounts/", include("accounts.urls")),
     path("forum/", include("forum.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
