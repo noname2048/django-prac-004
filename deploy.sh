18d1c7a1ee6843d990f207b9377bd0034f343975
noname2048 <sungwook.csw@gmail.com>
2021-03-24 01:18:02 +0900

    deploy.sh+pwd

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 # docker-compose -p gunicorn-app-blue -f dockerfiles/docker-compose.blue.yml up --build -d
 # docker-compose -p gunicorn-app-green -f dockerfiles/docker-compose.green.yml up --build -d
+echo "-->deploy.sh"
+echo ${PWD}
 
 DOCKER_APP_NAME=gunicorn-app
 EXIST_BLUE=$(docker-compose -p ${DOCKER_APP_NAME}-blue -f dockerfiles/docker-compose.blue.yml ps | grep Up)
