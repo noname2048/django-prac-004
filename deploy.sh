c1b9954f0d04597fb78863f6801dfd21e7933c10
noname2048 <sungwook.csw@gmail.com>
2021-03-30 00:28:00 +0900

    always build with no-cache in product

--- deploy.sh
+++ deploy.sh
@@ -10,14 +10,14 @@ EXIST_BLUE=$(docker-compose -p blue -f dockerfiles/docker-compose.blue.yml ps | 
 
 if [ -z "$EXIST_BLUE" ]; then
 	echo "blue up"
-	docker-compose -f dockerfiles/docker-compose.blue.yml -p blue up -d
+	docker-compose -f dockerfiles/docker-compose.blue.yml -p blue up --build -d
 	sleep 10
 	# TODO: if blue up, send slack to deploy success and green down
 	# TODO: else blue no up, send slack to deploy fails and send logs
 	docker-compose -f dockerfiles/docker-compose.green.yml -p green down
 else
 	echo "green up"
-	docker-compose -f dockerfiles/docker-compose.green.yml -p green up -d
+	docker-compose -f dockerfiles/docker-compose.green.yml -p green up --build -d
 	sleep 10
 	# TODO
 	docker-compose -f dockerfiles/docker-compose.blue.yml -p blue down
